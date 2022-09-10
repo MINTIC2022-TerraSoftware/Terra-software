b3adaad8403f493a5dcff0b75cf1810094887f94
nicolas_aponte <nft.explorer.blockchain@gmail.com>
2022-09-10 13:52:26 -0500

    feat: Se implementan controladores para vista de home, about, login y loguot.

--- src/main/java/com/mintic/terra_software/controller/HomeController.java
+++ src/main/java/com/mintic/terra_software/controller/HomeController.java
@@ -3,6 +3,8 @@ package com.mintic.terra_software.controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.servlet.http.HttpServletRequest;
+
 
 @org.springframework.stereotype.Controller
 
@@ -13,6 +15,35 @@ public class HomeController {
         model.addAttribute("mensaje", "Bienvenidos tripulantes");
         return "home";
     }
+
+    /**
+     * Metodo que muestra la vista de la pagina de Acerca
+     * @return
+     */
+    @GetMapping("/about")
+    public String mostrarAcerca() {
+        return "acerca";
+    }
+
+    /**
+     * Método que muestra el formulario de login personalizado.
+     * @return
+     */
+    @GetMapping("/login")
+    public String mostrarLogin() {
+        return "formLogin";
+    }
+
+    /**
+     * Método personalizado para cerrar la sesión del usuario
+     * @param request
+     * @return
+     */
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) {
+        return "redirect:/";
+    }
+
 
 }
 
